a4dd65f7db0c1df07eff65ff634e429f2c24daea
Liam Pulles <me@liampulles.com>
2021-06-03 20:09:01 +0200

    WIP

--- pkg/driver/cli/handler.go
+++ pkg/driver/cli/handler.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/liampulles/go-mastermind/pkg/usecase"
 )
@@ -30,11 +31,19 @@ type Command func(args []string) error
 
 func (h *HandlerImpl) resolveCommand(args []string) (Command, error) {
 	if len(args) < 1 {
-		return nil, fmt.Errorf("must provide a command")
+		return nil, fmt.Errorf("must provide a command. valid commands: %s", h.commandsDescription())
 	}
+	commandName := args[0]
+	commandMap := h.commandMap()
 }
 
-func commandDesc()
+func (h *HandlerImpl) commandsDescription() string {
+	var names []string
+	for k := range h.commandMap() {
+		names = append(names, k)
+	}
+	return strings.Join(names, ", ")
+}
 
 func (h *HandlerImpl) commandMap() map[string]Command {
 
